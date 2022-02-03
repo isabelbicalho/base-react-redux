ef7ed2c0c012770d50da837169fa7fb965bb5814
Isabel Bicalho Amaro <isabel.amaro@hotmart.com>
2022-02-03 18:57:41 -0300

    Add React project

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Tweet } from './components/Tweet';
+import { AppRoutes } from './Router';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [tweets, setTweets] = useState<string[]>([
+    'Tweet 1',
+    'Tweet 2',
+    'Tweet 3',
+    'Tweet 4',
+  ]);
+
+  //const [count, setCount] = useState(0)
+
+  function createTweet() {
+    console.log('Creating tweet');
+    setTweets([...tweets, 'Tweet 5'])
+  }
+
+  return (
+    <AppRoutes/>
+    // <div>
+    //   {
+    //     tweets.map(tweet => {
+    //       return <Tweet text={tweet} />
+    //     })
+    //   }
+
+    //   <button
+    //     onClick={createTweet}
+    //     style={{
+    //       backgroundColor: '#f0d5e1',
+    //       border: 0,
+    //       padding: '6px 12px'
+    //     }}
+    //     >
+    //       Add tweet
+    //     </button>
+    // </div>
+  )
+}
+
+export default App
